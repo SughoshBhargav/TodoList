58d5ff57dacb8d9599a49f0e6606999ce67df3c8
SughoshBhargav <sughoshsv@.com>
2023-05-18 00:11:44 +0530

    Updates

---
+++ precache-manifest.fee468173f6370a17fd1df1a9a751256.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41403c225d124656809b69b6d75ee07e",
+    "url": "/SughoshBhargav/TodoList.git/index.html"
+  },
+  {
+    "revision": "ad36e0aba4588e846032",
+    "url": "/SughoshBhargav/TodoList.git/static/js/2.05d131db.chunk.js"
+  },
+  {
+    "revision": "e18e547a2ccb6a2a0240",
+    "url": "/SughoshBhargav/TodoList.git/static/js/main.21d98691.chunk.js"
+  },
+  {
+    "revision": "2a1ebe4f6647d8d101cb",
+    "url": "/SughoshBhargav/TodoList.git/static/js/runtime-main.162e722a.js"
+  }
+]);
